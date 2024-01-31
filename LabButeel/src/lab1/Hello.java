f16032df5db4a58869f3b18d342cf8f6bf7e252d
Acer <Acer@Acer-PC>
2024-01-31 11:44:12 +0800

    Initial Commit.

---
+++ LabButeel/src/lab1/Hello.java
@@ -0,0 +1,10 @@
+package lab1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method type name = new type();
+		System.out.println("Hello world");
+	}
+
+}
